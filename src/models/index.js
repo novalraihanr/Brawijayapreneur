49b5d9ea828655746d8066cb315e1b25886568bb
Noval Raihan Ramadhan <Sleepyneko23@gmail.com>
2023-07-25 18:22:06 +0700

    Deletion

--- src/models/index.js
+++
@@ -1,7 +0,0 @@
-// index.js
-const db = require('../config/database');
-
-db.user = require('./user.model')(db.sequelize, db.Sequelize);
-db.article = require('./article.model')(db.sequelize, db.Sequelize);
-
-module.exports = db;
